ce48bea76d1774f8a3dc7d64846828fd814fb1f4
emittman <emittman@iastate.edu>
2017-08-20 14:44:10 -0500

    slurm script to run RDA

---
+++ R/RDA.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+#SBATCH --time=4-0:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=16
+#SBATCH --partition=gpu
+#SBATCH --error=msg/rda.err
+#SBATCH --output=msg/rda.out
+#SBATCH --mail-user=emittman@iastate.edu
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+
+module load R
+
+R CMD BATCH --vanilla --no-save RDA.R #run an R script using R
