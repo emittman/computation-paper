3011dbdb78e64c4447260c8d754891186e472042
emittman <emittman@iastate.edu>
2017-08-30 17:32:21 -0500

    Fix typos

--- R/bottleneck/script.R
+++ R/bottleneck/script.R
@@ -2,10 +2,10 @@ library(cudarpackage)
 library(dplyr)
 set.seed(24807923)
 source("../util/main-functions.R")
-load("/data/cuda-dat-ind.Rdata")
+load("/data/cuda-dat-std.Rdata")
 
 #run a pilot chain and reorder clusters
-inits <- initialize_chain(sample(1e8,1), K=2^10, n.iter=100, methodPi="Stick-breaking")
+inits <- initialize_chain(sample(1e8,1), K=2^10, n.iter=100, methodPi="stickBreaking")
 
 inits1 <- inits
 inits1$control$idx_save <- as.integer(0:(cuda_dat$G-1))
